8748de33e4009cbef0509a13ab6214dae01af91d
Senuka Chandunu <senukaacc@gmail.com>
2025-07-13 23:21:07 +0530

    init: iOS app

---
+++ StepCounter/StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Senuka Chandunu on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
